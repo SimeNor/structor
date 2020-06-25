910362a9c2526ceefe1265e9664cc232c549ef2a
EnvyPhD <twiller3@hotmail.com>
2020-06-25 13:24:48 +0200

    Fix: Moved patientview components to follow logical structure

--- src/dataexplorer/dataexplorer-application/src/components/MyPatients/MyPatients.tsx
+++ src/dataexplorer/dataexplorer-application/src/components/MyPatients/MyPatients.tsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 import { Row, Col } from 'antd';
 import Search from 'antd/lib/input/Search';
-import PatientInfo from '../PatientInfo/PatientInfo';
-const MyPatients = () => {
+import PatientInfo from './PatientInfo/PatientInfo';
+const MyPatients = (props: any) => {
+    const handleSearch = (value: any) => {
+        setPatientID(value);
+        // props.history.push({ pathname: 'Søk' });
+    };
+
     const [patientID, setPatientID] = useState();
     return (
         <>
@@ -11,7 +16,7 @@ const MyPatients = () => {
                 <Col span={300}>
                     <Search
                         placeholder="Søk etter en pasient!"
-                        onSearch={(value) => setPatientID(value)}
+                        onSearch={handleSearch}
                     />
                 </Col>
             </Row>
